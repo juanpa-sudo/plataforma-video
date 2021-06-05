426633b5447de5581c8489a6a37176c1acedd20f
Juan Pablo <jpcortesmacias@gmail.com>
2021-06-05 10:42:14 -0500

    Eslint

---
+++ src/components/HelloWord.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../assets/style/App.scss'
+
+function HelloWord() {
+    return (
+        <div>
+            <h1>Hola Mundo</h1>    
+        </div>
+    )
+}
+
+export default HelloWord
